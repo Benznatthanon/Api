eefa6369c89c1e9af526a34f31f9f667694f43f8
Natthanon <59160268@go.buu.ac.th>
2018-05-28 16:57:58 +0700

    first commit

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express()
-let contacts = []
+let contacts = [{
+    name : 'Natthanon',
+    phoneNumber : '0876714077'
+},{
+    name : 'Phanuwat',
+    phoneNumber : '0616189062'
+},{
+    name : 'Chatcha',
+    phoneNumber : '097190611'
+}
+]
 
 app.use(bodyParser.json())
 app.use(cors())
